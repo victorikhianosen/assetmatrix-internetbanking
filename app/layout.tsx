ac0724201b207df4195c12039b2ca35e52bf3e32
Adejumobi Ayotomiwa Samuel <adejusamuel@gmail.com>
2026-03-05 13:35:19 +0100

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,14 @@ import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
   subsets: ["latin"],
-  weight: ["100","200","300","400","500","600","700","800","900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-roboto",
 });
 
-
 export const metadata: Metadata = {
   title: "Asset Matrix - Internet Banking",
-  description: "Internet banking platform for asset matrix microfinance bank customers",
+  description:
+    "Access Asset Matrix Microfinance Bank's secure internet banking platform. Transfer funds, manage your account, and perform financial transactions easily online.",
   keywords: [
     "asset matrix",
     "internet banking",
@@ -22,12 +22,29 @@ export const metadata: Metadata = {
     "banking",
     "asset management",
     "financial services",
+    "financial technology",
     "Transfer",
-    "Loan",
     "Deposit",
     "Payment",
   ],
-  icons: "/favicon.ico",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/assets/images/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "Asset Matrix - Internet Banking",
+    description:
+      "Asset Matrix is an internet banking platform that provides microfinance services to customers in Nigeria. It offers a range of financial products and services, including loan, deposit, and payment options.",
+    url: "https://assetmatrixmfb.com/",
+    siteName: "Asset Matrix",
+    type: "website",
+    locale: "en_NG",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
