aa13b1306aed8645e8824e5532e197ee6dbd18c0
Adejumobi Ayotomiwa Samuel <adejusamuel@gmail.com>
2026-03-08 20:37:40 +0100

    solved styles problems

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -201,248 +201,251 @@ export default function RegisterPage() {
      UI (UNCHANGED)
   ================================ */
 
+  const inputStyle =
+    "w-full text-black rounded-lg border border-secondary/50 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary";
+
   return (
     <>
       <Loader show={loading} />
 
-      <div className="min-h-screen flex bg-[#F7F7F7]">
+      <div className="min-h-screen flex bg-muted">
         <SideBar />
 
-        <div className="w-full lg:w-1/2 flex items-center justify-center px-6">
+        <div className="w-full lg:w-1/2 flex items-center justify-center px-6 py-10">
           <div className="w-full max-w-3xl">
+
             {/* LOGO */}
             <div className="flex justify-center mb-8">
               <Image
+                alt="Logo"
                 src="/assets/images/logo.png"
-                alt="logo"
-                width={100}
+                width={200}
                 height={100}
                 className="h-12 object-contain"
               />
             </div>
 
             {/* TITLE */}
-            <div className="text-center mb-6">
-              <h2 className="text-xl font-semibold text-primary">Create your account</h2>
-              <p className="text-sm text-secondary/50">Open a secure digital bank account in minutes</p>
+            <div className="text-center mb-8">
+              <h2 className="text-xl font-semibold text-primary">
+                Create your account
+              </h2>
+              <p className="text-sm text-gray-500">
+                Open a secure digital bank account in minutes
+              </p>
             </div>
 
             {/* FORM */}
 
-            <form onSubmit={handleSubmit} className="space-y-4 grid grid-cols-2 gap-6">
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">First Name</label>
+            <form
+              onSubmit={handleSubmit}
+              className="grid grid-cols-1 md:grid-cols-2 gap-6"
+            >
+
+              {/* First Name */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  First Name
+                </label>
                 <input
                   name="first_name"
-                  placeholder="First name"
                   value={form.first_name}
-                  onChange={handleChange}
                   readOnly
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.first_name ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-                {errors.first_name && (
-                  <span className="text-red-600 text-md mt-1">{errors.first_name}</span>
-                )}{" "}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">Last Name</label>
+              {/* Last Name */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  Last Name
+                </label>
                 <input
                   name="last_name"
-                  placeholder="Last name"
                   value={form.last_name}
-                  onChange={handleChange}
                   readOnly
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.last_name ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-
-                {errors.username && (
-                  <span className="text-red-600 text-md mt-1">{errors.last_name}</span>
-                )}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">Phone Number</label>
+              {/* Phone */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  Phone Number
+                </label>
                 <input
                   name="phone"
-                  placeholder="Phone number"
-                  inputMode="numeric"
-                  maxLength={11}
                   value={form.phone}
                   readOnly
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.phone ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-                {errors.phone && <span className="text-red-600 text-md mt-1">{errors.phone}</span>}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">Email Address</label>
+              {/* Email */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  Email Address
+                </label>
                 <input
                   name="email"
                   type="email"
-                  placeholder="Email address"
                   value={form.email}
                   onChange={handleChange}
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.email ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-                {errors.email && <span className="text-red-600 text-md mt-1">{errors.email}</span>}
+                {errors.email && (
+                  <p className="text-red-600 text-sm mt-1">
+                    {errors.email}
+                  </p>
+                )}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">
-                  Bank Verification Number (BVN)
+              {/* BVN */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  BVN
                 </label>
                 <input
                   name="bvn"
-                  placeholder="Bank Verification Number (BVN)"
                   value={form.bvn}
                   readOnly
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.bvn ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-
-                {errors.bvn && <span className="text-red-600 text-md mt-1">{errors.bvn}</span>}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">Date of Birth</label>
+              {/* DOB */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  Date of Birth
+                </label>
                 <input
                   name="dob"
                   type="date"
                   value={form.dob}
-                  onChange={handleChange}
                   readOnly
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.dob ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-
-                {errors.dob && <span className="text-red-600 text-md mt-1">{errors.dob}</span>}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">Gender</label>
+              {/* Gender */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  Gender
+                </label>
                 <select
                   name="gender"
                   value={form.gender}
                   onChange={handleChange}
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.gender ? "border-red-600" : ""}`}>
+                  className={inputStyle}
+                >
                   <option value="">Select gender</option>
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
-                {errors.gender && (
-                  <span className="text-red-600 text-md mt-1">{errors.gender}</span>
-                )}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">Username</label>
+              {/* Username */}
+              <div>
+                <label className="text-sm font-medium text-primary">
+                  Username
+                </label>
                 <input
                   name="username"
-                  placeholder="Username"
                   value={form.username}
                   onChange={handleChange}
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${errors.username ? "border-red-600" : ""}`}
+                  className={inputStyle}
                 />
-                {errors.username && (
-                  <span className="text-red-600 text-md mt-1">{errors.username}</span>
-                )}
               </div>
-              <div className="col-span-2 md:col-span-1 relative">
-                <label className="block text-sm font-medium text-primary mb-1">Password</label>
+
+              {/* Password */}
+              <div className="relative">
+                <label className="text-sm font-medium text-primary">
+                  Password
+                </label>
                 <input
                   name="password"
                   type={showPassword ? "text" : "password"}
-                  placeholder="Password (min 8 characters)"
                   value={form.password}
                   onChange={handleChange}
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" pr-10 ${
-                    errors.password ? "border-red-600" : ""
-                  }`}
+                  className={inputStyle}
                 />
+
                 <span
                   onClick={() => setShowPassword(!showPassword)}
-                  className="absolute right-4 top-10 cursor-pointer text-secondary/50">
+                  className="absolute right-4 top-10 cursor-pointer text-gray-500"
+                >
                   {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
                 </span>
-                {errors.password && (
-                  <span className="text-red-600 text-md mt-1">{errors.password}</span>
-                )}
               </div>
 
-              <div className="col-span-2 md:col-span-1">
-                <label className="block text-sm font-medium text-primary mb-1">
+              {/* Confirm Password */}
+              <div>
+                <label className="text-sm font-medium text-primary">
                   Confirm Password
                 </label>
                 <input
                   name="confirm_password"
                   type="password"
-                  placeholder="Confirm Password (min 8 characters)"
                   value={form.confirm_password}
                   onChange={handleChange}
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" ${
-                    errors.confirm_password ? "border-red-600" : ""
-                  }`}
+                  className={inputStyle}
                 />
-
-                {errors.confirm_password && (
-                  <span className="text-red-600 text-md mt-1">{errors.confirm_password}</span>
-                )}
               </div>
 
-              <div className="col-span-2 md:col-span-1 relative">
-                <label className="block text-sm font-medium text-primary mb-1">PIN</label>
+              {/* PIN */}
+              <div className="relative">
+                <label className="text-sm font-medium text-primary">
+                  PIN
+                </label>
                 <input
                   name="pin"
                   type={showPin ? "text" : "password"}
-                  placeholder="PIN (4 digits)"
+                  maxLength={4}
                   value={form.pin}
                   onChange={handleChange}
-                  inputMode="numeric"
-                  maxLength={4}
-                  className={`"w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3" pr-10 ${
-                    errors.pin ? "border-red-600" : ""
-                  }`}
+                  className={inputStyle}
                 />
+
                 <span
                   onClick={() => setShowPin(!showPin)}
-                  className="absolute right-4 top-10 cursor-pointer text-gray-500">
+                  className="absolute right-4 top-10 cursor-pointer text-gray-500"
+                >
                   {showPin ? <EyeOff size={18} /> : <Eye size={18} />}
                 </span>
-                {errors.pin && <span className="text-red-600 text-md mt-1">{errors.pin}</span>}
               </div>
 
-              <div className="col-span-2 md:col-span-1 relative">
-                <label className="block text-sm font-medium text-primary mb-1">Confirm PIN</label>
+              {/* Confirm PIN */}
+              <div className="relative">
+                <label className="text-sm font-medium text-primary">
+                  Confirm PIN
+                </label>
                 <input
                   name="confirm_pin"
                   type={showConfirmPin ? "text" : "password"}
-                  placeholder="Confirm PIN (4 digits)"
+                  maxLength={4}
                   value={form.confirm_pin}
                   onChange={handleChange}
-                  inputMode="numeric"
-                  maxLength={4}
-                  className={`w-full text-black rounded-lg border-2 border-secondary/50 px-4 py-3 pr-10 ${
-                    errors.confirm_pin ? "border-red-600" : ""
-                  }`}
+                  className={inputStyle}
                 />
+
                 <span
                   onClick={() => setShowConfirmPin(!showConfirmPin)}
-                  className="absolute right-4 top-10 cursor-pointer text-gray-500">
+                  className="absolute right-4 top-10 cursor-pointer text-gray-500"
+                >
                   {showConfirmPin ? <EyeOff size={18} /> : <Eye size={18} />}
                 </span>
-                {errors.confirm_pin && (
-                  <span className="text-red-600 text-md mt-1">{errors.confirm_pin}</span>
-                )}
               </div>
 
+              {/* BUTTON */}
               <button
                 type="submit"
-                className="col-span-2 cursor-pointer
-                  w-full py-3 rounded-lg font-semibold bg-primary text-white hover:opacity-90 transition">
+                className="col-span-1 md:col-span-2 w-full py-3 rounded-lg font-semibold bg-primary text-white hover:opacity-90 transition"
+              >
                 Create Account
               </button>
             </form>
 
-            <p className="text-center text-xs text-gray-400 mt-4">
+            <p className="text-center text-xs text-gray-400 mt-6">
               By continuing, you agree to our Terms & Privacy Policy
             </p>
           </div>
